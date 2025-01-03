10667db018fc14947b689711755a712840c4c490
Marianna BIttencourt <marianna.bittencourt@combell.group>
2025-01-03 15:04:01 +0100

    Change return in AmbtaDoctrineEncryptBundle

--- src/AmbtaDoctrineEncryptBundle.php
+++ src/AmbtaDoctrineEncryptBundle.php
@@ -5,13 +5,12 @@ namespace Ambta\DoctrineEncryptBundle;
 use JetBrains\PhpStorm\Pure;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Ambta\DoctrineEncryptBundle\DependencyInjection\DoctrineEncryptExtension;
-
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\DoctrineExtension;
 class AmbtaDoctrineEncryptBundle extends Bundle
 {
     #[Pure]
     public function getContainerExtension(): ?ExtensionInterface
     {
-        return new DoctrineEncryptExtension();
+        return new DoctrineExtension();
     }
 }
